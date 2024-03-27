d13a4a54cdf646cb668367a5d6feeed346134644
alexquar <alexanderquarrie@gmail.com>
2024-03-27 09:49:46 -0400

    card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import SingleCard from './components/SingleCard';
 const cardImages = [
   { "src": "/img/helmet-1.png" },
   { "src": "/img/potion-1.png" },
@@ -25,12 +26,7 @@ function App() {
 
     <div className="card-grid">
       {cards.map(card => (
-        <div className="card" key={card.id}>
-          <div>
-            <img className="front" src={card.src} alt="card front" />
-            <img className="back" src="/img/cover.png" alt="cover" />
-          </div>
-        </div>
+       <SingleCard key={card.id} card={card} />
       ))}
     </div>
 
