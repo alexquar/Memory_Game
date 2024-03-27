972e370c9954fc9874ef27faee608f8fcf2d5cc7
alexquar <alexanderquarrie@gmail.com>
2024-03-27 10:44:47 -0400

    check cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import './App.css';
-import { useState } from 'react';
-import SingleCard from './components/SingleCard';
+import { useState, useEffect } from 'react'
+import './App.css'
+import SingleCard from './components/SingleCard'
+
 const cardImages = [
   { "src": "/img/helmet-1.png" },
   { "src": "/img/potion-1.png" },
@@ -15,15 +16,43 @@ function App() {
   const [turns, setTurns] = useState(0)
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
+
+  // shuffle cards for new game
   const shuffleCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages].sort( ()=> Math.random() - 0.5)
-    .map((card)=> ({...card, id: Math.random()}))
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }))
+      
     setCards(shuffledCards)
     setTurns(0)
   }
 
+  // handle a choice
   const handleChoice = (card) => {
+    console.log(card)
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+
+      if (choiceOne.src === choiceTwo.src) {
+        console.log('those cards match')
+        resetTurn()
+      } else {
+        console.log('those cards do not match')
+        resetTurn()
+      }
+
+    }
+  }, [choiceOne, choiceTwo])
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
   }
 
 
--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -4,17 +4,13 @@ export default function SingleCard({card, handleChoice}) {
 
     const handleClick = () => {
         handleChoice(card)
-    }
-
-
-
-
-  return (
-    <div className="card">
+      }
+      return (
+        <div className="card">
           <div>
             <img className="front" src={card.src} alt="card front" />
-            <img className="back" src="/img/cover.png" alt="cover" onClick={handleClick} />
+            <img className="back" src="/img/cover.png" onClick={handleClick} alt="cover" />
           </div>
         </div>
-  )
-}
+      )
+    }
